1000800dd7927f948a909fb9bb745b0e904f1cff
Luis Menendez <lmenendez.1@gmail.com>
2023-11-30 19:08:14 -0600

    Adding post method

--- src/dao/Users.dao.js
+++ src/dao/Users.dao.js
@@ -1,25 +1,26 @@
 import userModel from "./models/User.js";
 
-
 export default class Users {
-    
-    get = (params) =>{
-        return userModel.find(params);
-    }
+  get = (params) => {
+    return userModel.find(params);
+  };
 
-    getBy = (params) =>{
-        return userModel.findOne(params);
-    }
+  getBy = (params) => {
+    return userModel.findOne(params);
+  };
 
-    save = (doc) =>{
-        return userModel.create(doc);
-    }
+  save = (doc) => {
+    return userModel.create(doc);
+  };
 
-    update = (id,doc) =>{
-        return userModel.findByIdAndUpdate(id,{$set:doc})
-    }
+  update = (id, doc) => {
+    return userModel.findByIdAndUpdate(id, { $set: doc });
+  };
 
-    delete = (id) =>{
-        return userModel.findByIdAndDelete(id);
-    }
+  delete = (id) => {
+    return userModel.findByIdAndDelete(id);
+  };
+  save = (user) => {
+    return userModel.create(user);
+  };
 }
